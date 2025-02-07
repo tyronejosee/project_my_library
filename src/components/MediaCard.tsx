ec6f64d3f8f025b1d1f2818a8c5124331039914a
tyronejose <alt.tyronejose@gmail.com>
2025-02-07 14:16:35 -0300

    style: add resposive design for lists

--- src/components/MediaCard.tsx
+++ src/components/MediaCard.tsx
@@ -1,32 +1,43 @@
 "use client";
-
 import { IMedia } from "@/interfaces/media.interface";
-import { Card, Image } from "@heroui/react";
+import { Card, CardFooter, Image } from "@heroui/react";
+import React from "react";
 
 interface Props {
   media: IMedia;
   widthImage: number;
 }
 
-export default function MediaCard({ media, widthImage }: Props) {
+export default function MediaCard({
+  media,
+  widthImage,
+}: Props) {
   return (
     <article className="group">
-      <Card
-        isPressable
-        shadow="sm"
-        radius="lg"
-        className="relative border border-neutral-800 overflow-hidden"
-      >
-        <Image
-          src={media.image}
-          alt={media.folder_name}
-          title={media.folder_name}
-          height={256}
-          width={widthImage}
-          loading="lazy"
-          radius="none"
-          className="transform transition-transform duration-300 group-hover:scale-110"
-        />
+      <Card isBlurred radius="lg" className="h-full p-1 active:bg-neutral-900 transition-colors duration-1000">
+        <div className="relative border border-neutral-800 overflow-hidden rounded-xl mx-auto">
+          <span className="hidden group-hover:block absolute top-2 right-2 bg-primary text-neutral-950 px-2 py-1 rounded-lg text-xs z-50">
+            {media.type}
+          </span>
+          <Image
+            src={media.image}
+            alt={media.folder_name}
+            title={media.folder_name}
+            height={256}
+            width={widthImage}
+            loading="lazy"
+            radius="none"
+            className="transform transition-transform duration-300 group-hover:scale-105"
+          />
+          <span className="hidden group-hover:block absolute bottom-2 text-white px-2 py-1 rounded-lg text-xs z-50">
+          {media.created_at}
+          </span>
+        </div>
+        <CardFooter className="flex flex-col text-xs h-20 p-2">
+          <h2 className="line-clamp-2">{media.folder_name}</h2>
+          <p className="text-neutral-600">{media.genre}</p>
+          <p className="text-neutral-600">{media.location}</p>
+        </CardFooter>
       </Card>
     </article>
   );
--- src/components/MediaList.tsx
+++ src/components/MediaList.tsx
@@ -1,73 +1,27 @@
 "use client";
-import React from "react";
 import MediaCard from "./MediaCard";
 import { IMedia } from "@/interfaces/media.interface";
-import {
-  Modal,
-  ModalContent,
-  ModalHeader,
-  ModalBody,
-  ModalFooter,
-  Button,
-  useDisclosure,
-} from "@heroui/react";
 
 interface Props {
   results: IMedia[];
 }
 
 export default function MediaList({ results }: Props) {
-  const { isOpen, onOpen, onOpenChange } = useDisclosure();
-
   if (results.length === 0) {
     return <p className="text-gray-500">No se encontraron resultados.</p>;
   }
 
   return (
     <>
-      <section className="grid grid-cols-8 gap-2 px-14">
+      <section className="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-8 gap-2">
         {results.map((result, index) => (
-          <div key={index} onClick={onOpen} className="cursor-pointer">
-            <MediaCard media={result} widthImage={165} />
-          </div>
+          <MediaCard
+            key={index}
+            media={result}
+            widthImage={165}
+          />
         ))}
       </section>
-      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
-        <ModalContent>
-          {(onClose) => (
-            <>
-              <ModalHeader className="flex flex-col gap-1">Modal Title</ModalHeader>
-              <ModalBody>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pulvinar risus non
-                  risus hendrerit venenatis. Pellentesque sit amet hendrerit risus, sed porttitor
-                  quam.
-                </p>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pulvinar risus non
-                  risus hendrerit venenatis. Pellentesque sit amet hendrerit risus, sed porttitor
-                  quam.
-                </p>
-                <p>
-                  Magna exercitation reprehenderit magna aute tempor cupidatat consequat elit dolor
-                  adipisicing. Mollit dolor eiusmod sunt ex incididunt cillum quis. Velit duis sit
-                  officia eiusmod Lorem aliqua enim laboris do dolor eiusmod. Et mollit incididunt
-                  nisi consectetur esse laborum eiusmod pariatur proident Lorem eiusmod et. Culpa
-                  deserunt nostrud ad veniam.
-                </p>
-              </ModalBody>
-              <ModalFooter>
-                <Button color="danger" variant="light" onPress={onClose}>
-                  Close
-                </Button>
-                <Button color="primary" onPress={onClose}>
-                  Action
-                </Button>
-              </ModalFooter>
-            </>
-          )}
-        </ModalContent>
-      </Modal>
     </>
   );
 }
