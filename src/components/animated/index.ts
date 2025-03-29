401e76928abf6be8a39078e61288660dab47d8f9
tyronejose <alt.tyronejose@gmail.com>
2025-03-28 22:24:50 -0300

    feat: add noise component for animated grain effect on canvas

--- src/components/animated/index.ts
+++ src/components/animated/index.ts
@@ -1,1 +1,4 @@
+export { default as AnimatedContent } from "./AnimatedContent";
+export { default as DefaultTransition } from "./DefaultTransition";
+export { default as Noise } from "./Noise";
 export { default as RotatingText } from "./RotatingText";
