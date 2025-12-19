d9c1c7ebf5b36ca331c384cadb0336b227d90e7c
Tyrone Jose <alt.tyronejose@gmail.com>
2025-12-19 16:15:29 -0300

    chore: update project configuration and dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  images: {
-    domains: ["imgsrv.crunchyroll.com"],
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
